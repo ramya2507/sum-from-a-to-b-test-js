9a55caa5833d72dd7a9a77cf06c2f47b99322b53
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-27 05:29:33 +0000

    Added recursive sum function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN < toN){
+    return fromN +sum(fromN + 1,toN);
+  } else{
+  return fromN; //base case
+  }
 }
-
 module.exports = sum;
